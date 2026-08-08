88a2f0be1fd18e866b9af692574624fe2c2c543c
Prince Jamdagni <princejamdagni123@gmail.com>
2026-08-08 16:01:40 +0530

    number

---
+++ Number.java
@@ -0,0 +1,21 @@
+public class Number {
+    public static void main(String[] args) {
+        int[] numbers = {12, 7, 25, 8, 33, 4};
+
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                System.out.println(num + " is even");
+            } else {
+                System.out.println(num + " is odd");
+            }
+        }
+
+        int max = numbers[0];
+        for (int num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        System.out.println("Largest number: " + max);
+    }
+}
